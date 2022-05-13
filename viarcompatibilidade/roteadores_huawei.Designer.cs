1e9427b7ff819f852a34fe3d9b1139cd455fa20b
Matheus Henrique Luiz <matheus_henriluiz@hotmail.com>
2022-05-12 23:17:37 -0300

    att fundo app

--- viarcompatibilidade/roteadores_huawei.Designer.cs
+++ viarcompatibilidade/roteadores_huawei.Designer.cs
@@ -151,6 +151,7 @@ namespace viarcompatibilidade
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
